5929babf4b1c23a4687434dc306be54c42755174
kishanhitk <kishansharma1231@gmail.com>
2021-05-15 10:03:50 +0530

    💫 Add animation on page heading

--- components/SectionHeading.tsx
+++ components/SectionHeading.tsx
@@ -1,9 +1,9 @@
 import { Heading } from "@chakra-ui/react";
 import React from "react";
-
+import classes from "./SectionHeading.module.css";
 function SectionHeading({ text }: { text: string }) {
   return (
-    <Heading textAlign="center" mt={3}>
+    <Heading textAlign="center" className={classes.animatedText} mt={3}>
       {text}
     </Heading>
   );
